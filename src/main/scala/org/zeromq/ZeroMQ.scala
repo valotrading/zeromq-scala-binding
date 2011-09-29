861ecaef50f0116de3eea9e7a04f9002b396b858
Karim Osman <karim@iki.fi>
2011-09-29 20:48:37 +0300

    zeromq-scala-bindings: Add support for a few more functions
    The ones that are not yet implemented are marked 'ignore' in the test
    specification.
    
    Signed-off-by: Karim Osman <karim@iki.fi>

--- src/main/scala/org/zeromq/ZeroMQ.scala
+++ src/main/scala/org/zeromq/ZeroMQ.scala
@@ -1,13 +1,69 @@
 package org.zeromq
 
-import com.sun.jna.{Library, Native}
+import com.sun.jna._
+import com.sun.jna.ptr._
 
 object ZeroMQ {
+  /** Socket types */
+  val ZMQ_PAIR = 0
+  val ZMQ_PUB = 1
+  val ZMQ_SUB = 2
+  val ZMQ_REQ = 3
+  val ZMQ_REP = 4
+  val ZMQ_DEALER = 5
+  val ZMQ_ROUTER = 6
+  val ZMQ_PULL = 7
+  val ZMQ_PUSH = 8
+  val ZMQ_XPUB = 9
+  val ZMQ_XSUB = 10
+  /** Socket options */
+  val ZMQ_HWM = 1
+  val ZMQ_SWAP = 3
+  val ZMQ_AFFINITY = 4
+  val ZMQ_IDENTITY = 5
+  val ZMQ_SUBSCRIBE = 6
+  val ZMQ_UNSUBSCRIBE = 7
+  val ZMQ_RATE = 8
+  val ZMQ_RECOVERY_IVL = 9
+  val ZMQ_MCAST_LOOP = 10
+  val ZMQ_SNDBUF = 11
+  val ZMQ_RCVBUF = 12
+  val ZMQ_RCVMORE = 13
+  val ZMQ_FD = 14
+  val ZMQ_EVENTS = 15
+  val ZMQ_TYPE = 16
+  val ZMQ_LINGER = 17
+  val ZMQ_RECONNECT_IVL = 18
+  val ZMQ_BACKLOG = 19
+  val ZMQ_RECONNECT_IVL_MAX = 21
+  /** Built-in devices */
+  val ZMQ_STREAMER = 1
+  val ZMQ_FORWARDER = 2
+  val ZMQ_QUEUE = 3
+  /* Unix errors */
+  val EINVAL = 22
+  /** ZMQ errors */
+  val ZMQ_HAUSNUMERO = 156384712
+  val EFSM = ZMQ_HAUSNUMERO + 51
+  val ENOCOMPATPROTO = ZMQ_HAUSNUMERO + 52
+  val ETERM = ZMQ_HAUSNUMERO + 53
+
   def loadLibrary: ZeroMQ = {
     Native.loadLibrary("zmq", classOf[ZeroMQ]).asInstanceOf[ZeroMQ]  
   }
 }
 
 trait ZeroMQ extends Library {
+  def zmq_bind(socket: Pointer, endpoint: String): Int
+  def zmq_close(socket: Pointer): Int
+  def zmq_connect(scoket: Pointer, endpoint: String): Int
+  def zmq_device(device: Int, frontend: Pointer, backend: Pointer): Int
+  def zmq_errno: Int
+  def zmq_getsockopt(socket: Pointer, option_name: Int, option_value: Pointer, option_len: LongByReference): Int
+  def zmq_init(io_threads: Int): Pointer
+  def zmq_setsockopt(socket: Pointer, option_name: Int, option_value: Pointer, option_len: NativeLong): Int
+  def zmq_socket(context: Pointer, socket_type: Int): Pointer
+  def zmq_strerror(errnum: Int): String
+  def zmq_term(context: Pointer): Int
   def zmq_version(major: Array[Int], minor: Array[Int], patch: Array[Int]): Unit
 }
--- src/test/scala/org/zeromq/ZeroMQSpec.scala
+++ src/test/scala/org/zeromq/ZeroMQSpec.scala
@@ -1,19 +1,102 @@
 package org.zeromq
 
+import java.util.concurrent.{Executors, TimeUnit}
 import org.scalatest.BeforeAndAfter
 import org.scalatest.WordSpec
 import org.scalatest.matchers.MustMatchers
+import org.zeromq.ZeroMQ._
+import scala.util.Random
+
+import com.sun.jna._
+import com.sun.jna.ptr._
 
 class ZeroMQSpec extends WordSpec with MustMatchers with BeforeAndAfter {
   "ZeroMQ" must {
     var zmq: ZeroMQ = null
+    var endpoint: String = null
     before {
       zmq = ZeroMQ.loadLibrary
+      endpoint = "tcp://127.0.0.1:" + randomPort
     }
-    "implement zmq_version" in {
-      val (major, minor, patch) = (Array(1), Array(1), Array(1))
-      zmq.zmq_version(major, minor, patch)
-      (major(0), minor(0)) must equal((2, 1))
+    "zmq_bind" in {
+      val context = zmq.zmq_init(1)
+      val socket = zmq.zmq_socket(context, ZMQ_PUB)
+      zmq.zmq_bind(socket, endpoint) must equal(0)
+    }
+    "zmq_close" in { 
+      val context = zmq.zmq_init(1)
+      val socket = zmq.zmq_socket(context, ZMQ_PUB)
+      zmq.zmq_close(socket) must equal(0)
+    }
+    "zmq_connect" in {
+      val context = zmq.zmq_init(1)
+      val (pub, sub) = (
+        zmq.zmq_socket(context, ZMQ_PUB), 
+        zmq.zmq_socket(context, ZMQ_SUB)
+      )
+      zmq.zmq_bind(pub, endpoint)
+      zmq.zmq_connect(sub, endpoint) must equal(0)
+    }
+    "zmq_device" in {
+      val context = zmq.zmq_init(1)
+      val (frontend, backend) = (
+        zmq.zmq_socket(context, ZMQ_DEALER), 
+        zmq.zmq_socket(context, ZMQ_ROUTER)
+      )
+      zmq.zmq_bind(frontend, "tcp://127.0.0.1:" + randomPort)
+      zmq.zmq_bind(backend, "tcp://127.0.0.1:" + randomPort)
+      val executor = Executors.newSingleThreadScheduledExecutor
+      executor.schedule(new Runnable { def run { zmq.zmq_term(context) } }, 1, TimeUnit.SECONDS)
+      zmq.zmq_device(ZMQ_QUEUE, frontend, backend) must equal(-1)
+      zmq.zmq_errno must equal(ZeroMQ.ETERM)
+    }
+    "zmq_errno" in { 
+      zmq.zmq_init(-1)
+      zmq.zmq_errno must equal (EINVAL)
+    }
+    "zmq_getsockopt / zmq_setsockopt" in {
+      val context = zmq.zmq_init(1)
+      val socket = zmq.zmq_socket(context, ZMQ_PUB)
+      val (offset, sizeInBytes, optionValue) = (0, 8, 1234)
+      val value = new Memory(sizeInBytes) { setInt(offset, optionValue) }
+      val (length, lengthRef) = (new NativeLong(sizeInBytes), new LongByReference(sizeInBytes))
+      zmq.zmq_setsockopt(socket, ZMQ_RECOVERY_IVL, value, length) must equal(0)
+      zmq.zmq_getsockopt(socket, ZMQ_RECOVERY_IVL, value, lengthRef) must equal(0)
+      value.getInt(offset) must equal(optionValue)
+    }
+    "zmq_init" in { 
+      val context = zmq.zmq_init(1)
+      context must not be (null)
+    }
+    "zmq_msg_close" ignore { }
+    "zmq_msg_copy" ignore { }
+    "zmq_msg_data" ignore { }
+    "zmq_msg_init_data" ignore { }
+    "zmq_msg_init_size" ignore { }
+    "zmq_msg_move" ignore { }
+    "zmq_poll" ignore { }
+    "zmq_recv" ignore { }
+    "zmq_send" ignore { }
+    "zmq_socket" in { 
+      val context = zmq.zmq_init(1)
+      val socket = zmq.zmq_socket(context, ZMQ_PUB)
+      socket must not be (null)
+    }
+    "zmq_strerror" in { 
+      zmq.zmq_init(-1)
+      zmq.zmq_strerror(ETERM) must equal("Context was terminated")
+    }
+    "zmq_term" in { 
+      val context = zmq.zmq_init(1)
+      zmq.zmq_term(context) must equal (0)
+    }
+    "zmq_version" in {
+      val (major_x, minor_x, patch_x) = (Array(1), Array(1), Array(1))
+      val (major_y, minor_y, patch_y) = (Array(1), Array(1), Array(1))
+      zmq.zmq_version(major_x, minor_x, patch_x)
+      zmq.zmq_version(major_y, minor_y, patch_y)
+      (major_x(0), minor_x(0), patch_x(0)) must equal (major_y(0), minor_y(0), patch_y(0))
     }
   }
+  def randomPort = 1024 + new Random(System.currentTimeMillis).nextInt(4096)
 }
